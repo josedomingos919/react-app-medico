42c808183147141d8160dd585fdd1b9f2e1dc643
Macbook Air <macbookair@MacBook-Air-de-Macbook.local>
2022-01-16 01:06:28 +0100

    pagination in medicines

--- src/pages/medication/index.js
+++ src/pages/medication/index.js
@@ -5,23 +5,27 @@ import { tableData, formatData } from "./util";
 import { useApp } from "./../../context/app";
 import { toast } from "react-toastify";
 import { confirmAlert } from "react-confirm-alert";
+import { getPagination } from "../../utilities/functions";
 
 export function Medication() {
   const { medicineData, setMedicineData } = useApp();
   const [isLoading, setIsLoading] = useState(false);
+  const [medications, setMedications] = useState([]);
+  const [limit, setLimit] = useState(5);
+  const [page, setPage] = useState(1);
 
   const getMedicines = useCallback(async () => {
     setIsLoading(true);
-    setMedicineData([]);
+    setMedications([]);
 
     const responseData = await services.medicine.get();
 
     if (!responseData?.data?.success)
       toast.error("Falha ao carregar os dados!");
-    else setMedicineData(responseData?.data?.payload ?? []);
+    else setMedications(responseData?.data?.payload ?? []);
 
     setIsLoading(false);
-  }, [setMedicineData, setIsLoading]);
+  }, [setMedications, setIsLoading]);
 
   const handleDelete = useCallback(
     ({ id = "", medicamento = "" }) => {
@@ -52,19 +56,35 @@ export function Medication() {
   );
 
   useEffect(() => {
+    setMedicineData(
+      getPagination({
+        data: medications,
+        limit,
+        page,
+      })
+    );
+  }, [medications, limit, page]);
+
+  useEffect(() => {
     getMedicines();
   }, [getMedicines]);
 
   return (
     <AppContent>
       <Table
+        limit={limit}
+        page={page}
+        totalData={medicineData?.totalData}
+        totalPage={medicineData?.totalPage}
         isLoading={isLoading}
         title={tableData.title}
         subTitle={tableData.subTitle}
         fields={tableData.fields}
-        data={formatData(medicineData)}
+        data={formatData(medicineData?.data)}
         optios={tableData.optios}
         onDelete={handleDelete}
+        onChangeLimit={setLimit}
+        onChangePage={setPage}
       />
     </AppContent>
   );
