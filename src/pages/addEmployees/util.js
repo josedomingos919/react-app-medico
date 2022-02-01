035cd51765c1a73b7fd450029f98915067e67be2
josedomingos919 <josedomingos919@gmail.com>
2022-02-01 01:46:41 +0100

    validate onliny if field is full

--- src/pages/addEmployees/util.js
+++ src/pages/addEmployees/util.js
@@ -1,68 +1,72 @@
-import { isEmpty } from "../../utilities/functions";
+import { isEmpty } from '../../utilities/functions'
 
 export const dayWeeksInitialState = [
   {
-    day_week: "monday",
-    time_start: "",
-    time_end: "",
+    day_week: 'monday',
+    time_start: '',
+    time_end: '',
   },
   {
-    day_week: "tuesday",
-    time_start: "",
-    time_end: "",
+    day_week: 'tuesday',
+    time_start: '',
+    time_end: '',
   },
   {
-    day_week: "wednesday",
-    time_start: "",
-    time_end: "",
+    day_week: 'wednesday',
+    time_start: '',
+    time_end: '',
   },
   {
-    day_week: "thursday",
-    time_start: "",
-    time_end: "",
+    day_week: 'thursday',
+    time_start: '',
+    time_end: '',
   },
   {
-    day_week: "friday",
-    time_start: "",
-    time_end: "",
+    day_week: 'friday',
+    time_start: '',
+    time_end: '',
   },
   {
-    day_week: "saturday",
-    time_start: "",
-    time_end: "",
+    day_week: 'saturday',
+    time_start: '',
+    time_end: '',
   },
   {
-    day_week: "sunday",
-    time_start: "",
-    time_end: "",
+    day_week: 'sunday',
+    time_start: '',
+    time_end: '',
   },
-];
+]
 
 export const validateDayWeeks = (data = []) => {
-  const erros = {};
+  const erros = {}
 
   data.forEach(({ day_week, time_start, time_end }) => {
-    const obj = {};
+    const obj = {}
 
     //if (isEmpty(time_start)) obj.time_start = "*Obrigatório!";
     //if (isEmpty(time_end)) obj.time_end = "*Obrigatório!";
 
-    if(new Date(`2001-02-10 ${ time_start}`) >= new Date(`2001-02-10 ${ time_end}`)){
-      obj.time_start = "*Deve ser menor!";
-      obj.time_end = "*Deve ser maior!";
-    }
+    if (time_start || time_end)
+      if (
+        new Date(`2001-02-10 ${time_start}`) >=
+        new Date(`2001-02-10 ${time_end}`)
+      ) {
+        obj.time_start = '*Deve ser menor!'
+        obj.time_end = '*Deve ser maior!'
+      }
 
-    if (!isEmpty(obj)) erros[day_week] = obj;
-  });
+    if (!isEmpty(obj)) erros[day_week] = obj
+  })
 
-  return isEmpty(erros) ? false : erros;
-};
+  return isEmpty(erros) ? false : erros
+}
 
 export const getUserProfileValue = (val = false) =>
-  val ? "Equipe de atendimento" : "Médico Plantonista";
+  val ? 'Equipe de atendimento' : 'Médico Plantonista'
 
-export const getUserProfileValueByString = (val = "") =>
-  val === "Equipe de atendimento" ? true : false;
+export const getUserProfileValueByString = (val = '') =>
+  val === 'Equipe de atendimento' ? true : false
 
 export const getDayWeek = (day, data = []) =>
-  data.find((e) => e.day_week === day);
+  data.find((e) => e.day_week === day)
